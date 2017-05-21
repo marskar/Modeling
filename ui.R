33e1653d8cc52ff596afa169dc52c5d0efbb8414
marskar <marskar@gmail.com>
2017-05-21 18:52:04 -0400

    Added modeling scripts and 1st shiny app in progress

---
+++ ui.R
@@ -0,0 +1,38 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("f",
+                   "Select a value",
+                   min = 0,
+                   max = 1,
+                   value = 0.5)
+    ),
+        sliderInput("e",
+                "Select a value",
+                min = 0,
+                max = 1,
+                value = 0.5)
+     ),    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("odePlot")
+    )
+  )
+))
